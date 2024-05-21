04da3e80e59231876ab98f3275396c0f3cab654b
Shukurhon <skukurrespekt@gmail.com>
2024-05-21 14:29:23 +0500

    yangilandim

--- dastur/lib/main.dart
+++ dastur/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false,
-  home: S(),));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: S(),
+  ));
 }
-
 
 class S extends StatefulWidget {
   const S({super.key});
@@ -16,6 +17,14 @@ class S extends StatefulWidget {
 class _SState extends State<S> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Container(width: 400,height: 300,color: Colors.black,),),);
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 400,
+          height: 300,
+          color: Colors.red,
+        ),
+      ),
+    );
   }
 }
